ca34d9065293b35a2a1edd8bdac07709ac2dc41a
Awase <87275938+awase123@users.noreply.github.com>
2024-04-26 14:17:40 -0400

    Completed

--- etchASketch.js
+++ etchASketch.js
@@ -2,29 +2,38 @@ document.addEventListener("DOMContentLoaded", () => {
   const div = document.querySelector(".container");
   const button = document.querySelector("#btn1");
 
+  button.addEventListener("click", () => {
+    while (div.firstChild) {
+      div.removeChild(div.firstChild);
+    }
+    amountOfSquares();
+  });
+
   function randomColor() {
     return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
       Math.random() * 256
     )}, ${Math.floor(Math.random() * 256)})`;
   }
 
-  function randomLetter() {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-    const randomIndex = Math.floor(Math.random() * alphabet.length);
-    return alphabet[randomIndex];
-  }
-
-  for (let i = 0; i < 256; i++) {
-    newDiv = document.createElement("div");
-    div.appendChild(newDiv);
+  function createSquare() {
+    const newDiv = document.createElement("div");
     newDiv.classList.add("grid-item");
+    newDiv.style.backgroundColor = randomColor();
+    div.appendChild(newDiv);
+
+    newDiv.addEventListener("mouseenter", () => {
+      newDiv.style.backgroundColor = randomColor();
+    });
   }
 
-  const gridItems = document.querySelectorAll(".grid-item");
-  gridItems.forEach((item) => {
-    item.addEventListener("mouseenter", () => {
-      item.textContent = randomLetter();
-      item.style.backgroundColor = randomColor();
-    });
-  });
+  function amountOfSquares() {
+    let squares = prompt("Input the number of squares you want:");
+    if (squares > 100) {
+      alert("Input is too high!");
+      amountOfSquares();
+    }
+    for (let i = 0; i < squares; i++) {
+      createSquare();
+    }
+  }
 });
